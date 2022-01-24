eed4b288293e88438c113a1dabec85e7c8c38460
James Depret <jamesdepret@hotmail.com>
2022-01-24 09:59:48 +0100

    update Character to use same names as pngs

--- src/app/utils/character.ts
+++ src/app/utils/character.ts
@@ -11,24 +11,25 @@
 	 * - "-look-right", 
 	 * - "-look-up"
 	 */
- const createCharacter = (scene: Phaser.Scene, charName:string, x: number, y: number): Phaser.Physics.Arcade.Sprite =>{
+const createCharacter = (scene: Phaser.Scene, charName:string, x: number, y: number): Phaser.Physics.Arcade.Sprite =>{
     const character = scene.physics.add.sprite(x , y, charName, charName +'-standing1.png');
     createCharAnimation(scene,charName);
     character.anims.play(charName + '-look-down')
     return character;
 };
 
+// todo: update sprite texture file to use same names as the animations: so all using "look" instead of "standing"
 const createCharAnimation = (scene: Phaser.Scene, charName: string) => {
-    createAnimation(scene,charName, charName + '-look-down', 	charName + '-standing');
-    createAnimation(scene,charName, charName + '-look-left',  	charName + '-standing-left');
-    createAnimation(scene,charName, charName + '-look-right', 	charName + '-standing-right');
-    createAnimation(scene,charName, charName + '-look-up', 	    charName + '-standing-up');
+    createAnimation(scene,charName, charName + '-look-down');
+    createAnimation(scene,charName, charName + '-look-left');
+    createAnimation(scene,charName, charName + '-look-right');
+    createAnimation(scene,charName, charName + '-look-up');
 };
 
-const createAnimation = (scene: Phaser.Scene, charname: string, key: string, pngName: string) =>{
+const createAnimation = (scene: Phaser.Scene, charname: string, key: string) =>{
     scene.anims.create({
         key: key,
-        frames: scene.anims.generateFrameNames(charname, { start: 1, end: 2, prefix: pngName, suffix:'.png'}),
+        frames: scene.anims.generateFrameNames(charname, { start: 1, end: 2, prefix: key, suffix:'.png'}),
         repeat: -1,
         frameRate: 4
     });
