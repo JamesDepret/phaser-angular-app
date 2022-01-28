ce61a83ef5bff340ecb413807e412fda50da24d3
James Depret <jamesdepret@hotmail.com>
2022-01-28 20:25:25 +0100

    improve movement

--- src/app/game/character/character.ts
+++ src/app/game/character/character.ts
@@ -1,3 +1,4 @@
+
 import { createCharacter } from "src/app/utils/character";
 import { Coordinate } from "src/app/utils/coordinate";
 import CombatMenu from "../menu/combat_menu";
@@ -217,26 +218,20 @@ export default class Character  {
 	private lastMoveUp: boolean = false;
     private lastMoveLeft: boolean = false;
     private lastMoveRight: boolean = false;
+    private lastMoveDown: boolean = false;
 	private spaceDown: boolean = false;
     private lastKnowAllowedCoordinate: Coordinate;
-	public setCursorValidation(cursors: Phaser.Types.Input.Keyboard.CursorKeys ){
-        
-        if(cursors.space?.isDown && this.spaceDown == false){
-			this.spaceDown = true;
-			this.MenuOpened = !this.MenuOpened;
-			let EnemyNear = false; // TODO: set value based on range to enemy
-			this.combatMenu.toggleCombatMenu(this.MenuOpened, EnemyNear); 
-		} else if (cursors.space?.isUp && this.spaceDown) {
-			this.spaceDown = false;
-		}
+    private urdlCoordinates: (Coordinate | null)[]=[null,null,null,null]
+	public setCursorValidation(cursors: Phaser.Types.Input.Keyboard.CursorKeys ){   
+        this.handleSpaceBar(cursors);
 
 		if(this.MenuOpened){
 			this.combatMenu.cursorInput(cursors);
             this.movement(false);
 		} else {
             this.movement(true);
-            let xPos = this.round32(this.sprite.x);
-            let yPos = this.round32(this.sprite.y);
+            let xPos = this.round32(this.sprite.x +16);
+            let yPos = this.round32(this.sprite.y + 16);
             
             let xCoord = (xPos + 16)/32 - 1.5;
             let yCoord = (yPos + 16)/32 - 1.5;
@@ -244,64 +239,102 @@ export default class Character  {
             let CoordinateInList = this.movableCoordinates.find(c => c.x == xCoord && c.y == yCoord );
             if(CoordinateInList) this.lastKnowAllowedCoordinate = CoordinateInList;
 
-            console.log(xCoord + " ; " + yCoord);
 			if(cursors.left?.isDown && CoordinateInList){
-                this.sprite.setVelocity(-this.SPRITESPEED,0);
-                this.lastMoveUp = false;
-                this.lastMoveLeft = true;
-                this.lastMoveRight = false;
-                this.sprite.anims.play(this.name + '-look-left', true);
+                if(this.urdlCoordinates[3] == null && !this.lastMoveLeft || this.lastMoveLeft && this.urdlCoordinates[3] != null) {
+                    this.urdlCoordinates = [null,null,null,this.movableCoordinates.find(c => c.x ==xCoord - 1 && c.y ==yCoord) ?? null];
+                }
+                
+                this.setDirection(false, false, false, true);
+                if(this.urdlCoordinates[3]){
+                    this.sprite.setVelocity(-this.SPRITESPEED,0);
+                    this.sprite.anims.play(this.name + '-look-left', true);
+                }
 			} 
 			else if (cursors.right?.isDown && CoordinateInList) {
-                this.sprite.setVelocity(this.SPRITESPEED, 0);
-                this.lastMoveUp = false;
-                this.lastMoveLeft = false;
-                this.lastMoveRight = true;
-                this.sprite.anims.play(this.name + '-look-right', true);
+                if(this.urdlCoordinates[1] == null && !this.lastMoveRight || this.urdlCoordinates[1] != null && this.lastMoveRight) {
+                    this.urdlCoordinates = [null,this.movableCoordinates.find(c => c.x == xCoord + 1 && c.y ==yCoord) ?? null, null,null];
+                }                
+                
+                this.setDirection(false, true, false, false);
+                if(this.urdlCoordinates[1]){
+                    this.sprite.setVelocity(this.SPRITESPEED, 0);
+                    this.sprite.anims.play(this.name + '-look-right', true);
+                }
 			} 
 			else if(cursors.down?.isDown && CoordinateInList){
-                this.sprite.setVelocity(0,this.SPRITESPEED);
-                this.lastMoveUp = false;
-                this.lastMoveLeft = false;
-                this.lastMoveRight = false;
-                this.sprite.anims.play(this.name + '-look-down', true);
+                if(this.urdlCoordinates[2] == null && !this.lastMoveDown || this.urdlCoordinates[2] != null && this.lastMoveDown) {
+                    this.urdlCoordinates = [null,null,this.movableCoordinates.find(c => c.x == xCoord && c.y == yCoord+1) ?? null,null];
+                }
+                this.setDirection(false, false, true, false);
+                if(this.urdlCoordinates[2]){
+                    this.sprite.setVelocity(0,this.SPRITESPEED);
+                    this.sprite.anims.play(this.name + '-look-down', true);
+                }
 			} 
-			else if (cursors.up?.isDown && CoordinateInList) {
-                this.sprite.setVelocity(0, -this.SPRITESPEED);
-                this.lastMoveUp = true;
-                this.lastMoveLeft = false;
-                this.lastMoveRight = false;
-                this.sprite.anims.play(this.name + '-look-up', true);
-			} else {this.sprite.setVelocity(0);
-                let xPosition = this.sprite.x;
-                let yPosition = this.sprite.y;
-                if ((xPosition + 16) % 32 != 0) {
-                    if(this.lastMoveLeft) { this.sprite.setX(this.round32(xPosition - 24) + 16 ); }
-                    if(this.lastMoveRight) { this.sprite.setX(this.round32(xPosition + 24) - 16 ); }
+			else if (cursors.up?.isDown && CoordinateInList) {      
+                if(this.urdlCoordinates[0] == null && !this.lastMoveUp || this.urdlCoordinates[0] != null && this.lastMoveUp) {
+                    this.urdlCoordinates = [this.movableCoordinates.find(c => c.x == xCoord && c.y == yCoord-1) ?? null,null,null,null];
                 }
-        
-                if ((yPosition + 16) % 32 != 0) {
-                    if(this.lastMoveLeft) { this.sprite.setY(this.round32(yPosition - 24) + 16 ); }
-                    if(!this.lastMoveRight && !this.lastMoveLeft && !this.lastMoveUp) { this.sprite.setY(this.round32(yPosition + 24) - 16 ); }
+                          
+                this.setDirection(true, false, false, false);
+                if(this.urdlCoordinates[0]){
+                    this.sprite.setVelocity(0, -this.SPRITESPEED);
+                    this.sprite.anims.play(this.name + '-look-up', true);
                 }
-                xPosition = this.sprite.x;
-                yPosition = this.sprite.y;
-                let xCoord = (this.round32(xPosition) + 16) /32 - 1.5;
-                let yCoord = (this.round32(yPosition) + 16)/32 - 1.5;
-                let CoordinateInList = this.movableCoordinates.find(c => c.x == xCoord && c.y == yCoord );
-                if(!CoordinateInList) {
-                    console.log('coordinateNotInList')
-                    if(this.lastMoveUp){ this.sprite.setY(this.lastKnowAllowedCoordinate.y * 32 + 16); }
-                    else if( this.lastMoveLeft ){ this.sprite.setX(this.lastKnowAllowedCoordinate.x * 32 + 16); }
-                    else if( this.lastMoveRight){ this.sprite.setX(this.lastKnowAllowedCoordinate.x * 32 + 16); }
-                    else { this.sprite.setY(this.lastKnowAllowedCoordinate.y * 32 +16 ); }
-                }
+			} else {
+                this.correctPosition();
 			}
 		}
 	}
 
+    private setDirection(up: boolean, right: boolean, down: boolean, left: boolean) {
+        this.lastMoveUp = up;
+        this.lastMoveLeft = left;
+        this.lastMoveRight = right;
+        this.lastMoveDown = down;
+    }
+
+    private correctPosition() {
+        this.sprite.setVelocity(0);
+        let xPosition = this.sprite.x;
+        let yPosition = this.sprite.y;
+        if ((xPosition + 16) % 32 != 0) {
+            if (this.lastMoveLeft) { this.sprite.setX(this.round32(xPosition - 24) + 16); }
+            if (this.lastMoveRight) { this.sprite.setX(this.round32(xPosition + 24) - 16); }
+            this.sprite.setY(this.round32(yPosition - 24) + 16);
+        }
+
+        if ((yPosition + 16) % 32 != 0) {
+            if (this.lastMoveLeft) { this.sprite.setY(this.round32(yPosition - 24) + 16); }
+            if (!this.lastMoveRight && !this.lastMoveLeft && !this.lastMoveUp) { this.sprite.setY(this.round32(yPosition + 24) - 16); }
+            this.sprite.setX(this.round32(xPosition - 24) + 16);
+        }
+        xPosition = this.sprite.x;
+        yPosition = this.sprite.y;
+        let xCoord = (this.round32(xPosition) + 16) / 32 - 1.5;
+        let yCoord = (this.round32(yPosition) + 16) / 32 - 1.5;
+        let CoordinateInList = this.movableCoordinates.find(c => c.x == xCoord && c.y == yCoord);
+        if (!CoordinateInList) {
+            if (this.lastMoveUp) { this.sprite.setY(this.lastKnowAllowedCoordinate.y * 32 + 16); }
+            else if (this.lastMoveLeft) { this.sprite.setX(this.lastKnowAllowedCoordinate.x * 32 + 16); }
+            else if (this.lastMoveRight) { this.sprite.setX(this.lastKnowAllowedCoordinate.x * 32 + 16); }
+            else { this.sprite.setY(this.lastKnowAllowedCoordinate.y * 32 + 16); }
+        }
+    }
+
+    private handleSpaceBar(cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
+        if (cursors.space?.isDown && this.spaceDown == false) {
+            this.spaceDown = true;
+            this.MenuOpened = !this.MenuOpened;
+            let EnemyNear = false; // TODO: set value based on range to enemy
+            this.combatMenu.toggleCombatMenu(this.MenuOpened, EnemyNear);
+        } else if (cursors.space?.isUp && this.spaceDown) {
+            this.spaceDown = false;
+        }
+    }
+
 	round32(value: number): number{
 		return 32 * (Math.round(value / 32));
-	}
+	} 
 
 }
